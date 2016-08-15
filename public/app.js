4c1ba9449b02c06735349886f84c5ae106228572
Andre Moeller <andre_g_moeller@yahoo.de>
2016-08-15 22:31:14 +0200

    Removes alert from learnjs.showView

--- public/app.js
+++ public/app.js
@@ -2,6 +2,5 @@
 var learnjs = {};
 learnjs.showView = function(hash) {
     var problemView = $('<div class="problem-view">').text('Coming soon!');
-    alert($('.view-container').length)
     $('.view-container').empty().append(problemView);
 };
